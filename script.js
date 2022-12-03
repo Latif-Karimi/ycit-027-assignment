ffa2cae365b9123475018f9e9d6530a1d7b3bd8e
Your Name <you@example.com>
2022-12-02 22:09:16 -0500

    ycit-027-assignment

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function getCatImage() {
     fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => {
+        .then(response => 
             // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
-        })
+            response.json())
+        
         .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
+            
+            let catsImgUrl= data[0].url
+
+            let catsImagEl = document.createElement("img")
+            catsImagEl.setAttribute("src", `${catsImgUrl}`)
+            
+            catsImagEl.classList.add("imageSize")
+
+            let imageContain = document.querySelector(".imageContain")
+            imageContain.appendChild(catsImagEl)
         });
 }
